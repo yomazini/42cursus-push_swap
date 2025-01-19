f5f9cb2362f09f5a4e3e4fd0c27ee4e496aaae7a
Youssef Mazini <maziniyoussef29@gmail.com>
2025-01-19 16:18:20 +0100

    19-01 2.0

---
+++ main/maintest.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+	write(2,"ko",2);
+	if (1)
+		exit(0);
+}
--- main/tools.c
+++ main/tools.c
@@ -81,14 +81,14 @@ long	ft_atol(char *str, t_stack_node **a)
 	return (sign * res);
 }
 
-// void    ft_putstr(char *str)
-// {
-//         while(*str)
-//         {
-//                 write(1,str,1);
-//                 str++;
-//         }
-// }
+void    ft_putstr(char *str)
+{
+        while(*str)
+        {
+                write(1,str,1);
+                str++;
+        }
+}
 
 void	free_error(t_stack_node **a)
 {
@@ -152,16 +152,198 @@ int len_stack(t_stack_node *stack)
 
 //find min in stack 
 
-t_stack_node *find_min(t_stack_node *stack)
+t_stack_node *find_min_node(t_stack_node *stack)
 {
-	
+	t_stack_node *min_node;
+	long min_number;
+
+	min_number = INT_MAX + 987;
+	if(!stack)
+		return NULL;
+	while (stack)
+	{
+		if (stack->value < min_number)
+		{
+			min_number = stack->value;
+			min_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (min_node);
 }
 
 //find max in stack
 
-//sort 2 in stack
+t_stack_node *find_max_node(t_stack_node *stack)
+{
+	t_stack_node *max_node;
+	long max_number;
+
+	max_number = INT_MIN - 6;
+	if (!stack)
+		return (NULL);
+	while(stack)
+	{
+		if (max_number > stack->value)
+		{
+			max_number = stack->value;
+			max_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (max_node);
+}
+
+
+
+//TODO: this pomodoro (finish all moves.     <rotate &&  rev_rotate>   < | sa sb ss | ra rb rr | rra rrb rrr | >)
+
+void	ft_swap_node(t_stack_node **head)
+{
+	t_stack_node *tmp;
+	t_stack_node *prev_1;
+	t_stack_node *next_1;
+
+	if (!(*head) || !((*head)->next))
+		return ;
+
+	tmp = *head;
+	prev_1 = (*head)->previous;
+	next_1 = (*head)->next->next;
+
+	(*head) = (*head)->next; // Update head to point to the second node
+	(*head)->next = tmp; // Update new head's next to point to the old head
+	(*head)->previous = prev_1; // Update new head's prev to point to the old head's prev
+
+	tmp->next = next_1; // Update old head's next to point to next_1
+	tmp->previous = *head; // Update old head's prev to point to the new head
+
+	if (next_1) // Update next_1's previous pointer if it exists
+		next_1->previous = tmp;
+	if (prev_1) // Update prev_1's next pointer if it exists
+		prev_1->next = *head;
+}
+
+
+void	ft_rotate_node(t_stack_node **head)
+{
+	t_stack_node *last;
+	t_stack_node *first_head;
+	first_head = head;
+	if (!*head || !(*head)->next)
+		return ;
+	last = last_node(head);
+	last->next = (*head);
+	first_head = first_head->next;
+	(*head)->next = NULL;
+	(*head)->previous = last;
+
+}
+
+void	ft_rev_rotate_node(t_stack_node **head)
+{
+
+}
+
+//finish all sa sb ss
+void	sa(t_stack_node **a, int testing)
+{
+	ft_swap_node(a);
+	if (!testing)
+		ft_putstr("sa\n");
+}
+
+void	sb(t_stack_node **b, int testing)
+{
+	ft_swap_node(b);
+	if (!testing)
+		ft_putstr("sa\n");
+}
+
+void	ss(t_stack_node **a, t_stack_node **b,int testing)
+{
+
+	ft_swap_node(a);
+	ft_swap_node(b);
+	if (!testing)
+		ft_putstr("ss\n");
+}
+
+/*
+rotate fun 
+*/
+
+
+void	ra(t_stack_node **a, int testing)
+{
+	ft_rotate_node(*a);
+	if(!testing)
+		ft_putstr("ra\n");
+
+}
+
+void	rb(t_stack_node **b, int testing)
+{
+	ft_rotate_node(*b);
+	if(!testing)
+		ft_putstr("rb\n");
+}
+
+void	rr(t_stack_node **a, int testing)
+{
+	ft_rotate_node(*a);
+	ft_rotate_node(*b);
+	if(!testing)
+		ft_putstr("rr\n");
+}
+
+/*
+rev rotate fun
+*/
+
+void	rra(t_stack_node **a, int testing)
+{
+	ft_rev_rotate_node(*a);
+	if(!testing)
+		ft_putstr("rra\n");
+
+}
+
+void	rrb(t_stack_node **b, int testing)
+{
+	ft_rev_rotate_node(*b);
+	if(!testing)
+		ft_putstr("rrb\n");
+
+}
+
+void	rrr(t_stack_node **a, t_stack_node **b, int testing)
+{
+	ft_rev_rotate_node(*a);
+	ft_rev_rotate_node(*b);
+	if(!testing)
+		ft_putstr("rrr\n");
+}
+
+
+/*
+		--> Split here move from the libft with edition and put it here as well;
+		--> 
+*/
 
 //sort 3 in stack
+
+void	sort_three(t_stack_node **a) // our goal to move biggest to botthom in first two condition
+{
+	t_stack_node *biggest;
+	biggest = find_max_node(a);
+	if (biggest == (*a))
+		ra(*a, 0);
+	else if (biggest == (*a)->next)
+		rra(*a, 0);
+	if ((*a)->value > (*a)->next->value)
+		sa(*a, 0);
+}
 
 
 t_stack_node *last_node(t_stack_node *stack)
