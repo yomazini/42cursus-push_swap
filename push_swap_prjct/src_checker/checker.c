87acfe3b5bd63cc7c51f95ba7f4fae812f590351
Youssef Mazini <maziniyoussef29@gmail.com>
2025-02-01 23:17:17 +0100

    fixing the norms of bonus not 100% sure if still correct -- 2.0

--- push_swap_prjct/src_checker/checker.c
+++ push_swap_prjct/src_checker/checker.c
@@ -6,81 +6,93 @@
 /*   By: ymazini <ymazini@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 22:22:36 by ymazini           #+#    #+#             */
-/*   Updated: 2025/02/01 22:47:52 by ymazini          ###   ########.fr       */
+/*   Updated: 2025/02/01 23:08:51 by ymazini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-static void parse_command(t_stack_node **a, t_stack_node **b, char *command)
+static void	parse_command(t_stack_node **a, t_stack_node **b, char *command)
 {
-    if (!ft_strncmp(command, "pa\n", 3))
-        pa(a, b, 1);  // Changed testing to 1
-    else if (!ft_strncmp(command, "pb\n", 3))
-        pb(b, a, 1);
-    else if (!ft_strncmp(command, "sa\n", 3))
-        sa(a, 1);
-    else if (!ft_strncmp(command, "sb\n", 3))
-        sb(b, 1);
-    else if (!ft_strncmp(command, "ss\n", 3))
-        ss(a, b, 1);
-    else if (!ft_strncmp(command, "ra\n", 3))
-        ra(a, 1);
-    else if (!ft_strncmp(command, "rb\n", 3))
-        rb(b, 1);
-    else if (!ft_strncmp(command, "rr\n", 3))
-        rr(a, b, 1);
-    else if (!ft_strncmp(command, "rra\n", 4))
-        rra(a, 1);
-    else if (!ft_strncmp(command, "rrb\n", 4))
-        rrb(b, 1);
-    else if (!ft_strncmp(command, "rrr\n", 4))
-        rrr(a, b, 1);
-    else
-        error(a, b, command);
+	if (!ft_strncmp(command, "pa\n", 3))
+		pa(a, b, 1);
+	else if (!ft_strncmp(command, "pb\n", 3))
+		pb(b, a, 1);
+	else if (!ft_strncmp(command, "sa\n", 3))
+		sa(a, 1);
+	else if (!ft_strncmp(command, "sb\n", 3))
+		sb(b, 1);
+	else if (!ft_strncmp(command, "ss\n", 3))
+		ss(a, b, 1);
+	else if (!ft_strncmp(command, "ra\n", 3))
+		ra(a, 1);
+	else if (!ft_strncmp(command, "rb\n", 3))
+		rb(b, 1);
+	else if (!ft_strncmp(command, "rr\n", 3))
+		rr(a, b, 1);
+	else if (!ft_strncmp(command, "rra\n", 4))
+		rra(a, 1);
+	else if (!ft_strncmp(command, "rrb\n", 4))
+		rrb(b, 1);
+	else if (!ft_strncmp(command, "rrr\n", 4))
+		rrr(a, b, 1);
+	else
+		error(a, b, command);
 }
 
-static void init_var(t_stack_node **stack_a, t_stack_node **stack_b, int *len)
+static int	check_empty_arguments(int ac, char **av)
 {
-    *len = 0;
-    *stack_a = NULL;
-    *stack_b = NULL;
+	int	i;
+
+	i = 1;
+	while (i < ac)
+	{
+		if (av[i][0] == '\0')
+		{
+			write(2, "Error\n", 6);
+			exit(1);
+		}
+		i++;
+	}
+	return (1);
 }
 
-int main(int ac, char **av)
+static void	process_commands(t_stack_node **a, t_stack_node **b)
 {
-    t_stack_node  *stack_a;
-    t_stack_node  *stack_b;
-    int     len;
-    char    *next_line;
+	char	*next_line;
 
-    if (ac > 1)
-    {
-        for (int i = 1; i < ac; i++)
-        {
-            if (av[i][0] == '\0')
-            {
-                write(2, "Error\n", 6);
-                exit(1);
-            }
-        }
-        init_var(&stack_a, &stack_b, &len);
-        initialize_stack(&stack_a, ac, av);
-        if (!stack_a || has_duplicates(stack_a))
-            ft_error(&stack_a, NULL);
-        len = stack_length(stack_a);
-        next_line = get_next_line(0);
-        while (next_line)
-        {
-            parse_command(&stack_a, &stack_b, next_line);
-            free(next_line);
-            next_line = get_next_line(0);
-        }
-        if (stack_sorted(stack_a) && stack_b == NULL)
-            write(1, "OK\n", 3);
-        else
-            write(1, "KO\n", 3);
-        free_stack(&stack_a);
-        free_stack(&stack_b);
-    }
+	next_line = get_next_line(0);
+	while (next_line)
+	{
+		parse_command(a, b, next_line);
+		free(next_line);
+		next_line = get_next_line(0);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	t_stack_node	*stack_a;
+	t_stack_node	*stack_b;
+	int				len;
+
+	if (ac > 1)
+	{
+		check_empty_arguments(ac, av);
+		len = 0;
+		stack_a = NULL;
+		stack_b = NULL;
+		initialize_stack(&stack_a, ac, av);
+		if (!stack_a || has_duplicates(stack_a))
+			ft_error(&stack_a, NULL);
+		len = stack_length(stack_a);
+		process_commands(&stack_a, &stack_b);
+		if (stack_sorted(stack_a) && stack_b == NULL)
+			write(1, "OK\n", 3);
+		else
+			write(1, "KO\n", 3);
+		free_stack(&stack_a);
+		free_stack(&stack_b);
+	}
+	return (0);
 }
